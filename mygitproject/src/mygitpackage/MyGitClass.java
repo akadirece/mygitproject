9a74a5444ccee2460d368cc5ab51e1e06e2ff47b
akadirece <akadirece1@gmail.com>
2021-05-05 12:06:16 +0200

    ilk commitim

--- mygitproject/src/mygitpackage/MyGitClass.java
+++ mygitproject/src/mygitpackage/MyGitClass.java
@@ -1,10 +1,10 @@
-package mygitproject;
+package mygitpackage;
 
 public class MyGitClass {
 
 	
 	
 	public static void main(String[] args) {
-		System.out.println("Version3");
+		System.out.println("Version1  ");
 	}
 }
--- mygitproject/src/mygitproject2/package-info.java
+++
@@ -1,1 +0,0 @@
-package mygitproject2;

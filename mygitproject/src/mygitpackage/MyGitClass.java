000e760fb313417e0beba98277901aee8480cb24
akadirece <akadirece1@gmail.com>
2021-05-05 12:34:00 +0200

    versiyon 3

--- mygitproject/src/mygitpackage/MyGitClass.java
+++ mygitproject/src/mygitpackage/MyGitClass.java
@@ -7,5 +7,6 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version1  ");
 		System.out.println("My second commit");
+		System.out.println("MyThird Commit");
 	}
 }
